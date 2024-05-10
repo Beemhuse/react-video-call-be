f062506bfeaf8368002c5e943361001e2f4dea7f
Beemhuse <brightawah94@gmail.com>
2024-05-10 07:02:29 -0700

    added cors middleware

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const http = require('http');
 const express = require('express');
+const cors = require('cors'); // Import the cors middleware
 const config = require('./config');
 const socket = require('./lib/socket');
 
 const app = express();
 
+app.use(cors()); // Use the cors middleware
 
 
 const server = http.createServer(app);
 
 // app.use('/', express.static(`${__dirname}/../client/dist`));
-
+app.use(cors({
+  origin: '*' // Allow requests only from http://example.com
+}));
 server.listen(config.PORT, () => {
   socket(server)
   console.log('Server is listening at :', config.PORT);
