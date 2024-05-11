b775a66a21daa04d7fc7e73dd3ba0150594ecffc
Beemhuse <brightawah94@gmail.com>
2024-05-11 10:51:41 -0700

    server

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const http = require('http');
-const express = require('express');
-const cors = require('cors'); // Import the cors middleware
 const config = require('./config');
 const socket = require('./lib/socket');
 
-const app = express();
-
-app.use(cors({
-  origin: '*' // Allow requests only from http://example.com
-}));
 
 
 const server = http.createServer(app);
